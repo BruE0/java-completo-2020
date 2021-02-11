cb386cb9662b383b2338608de0e364073133ad46
BruE0 <bruno7497@gmail.com>
2021-02-11 01:55:31 -0300

    Implemented UPDATE

--- javaCompleto2020JDBC/src/application/Program.java
+++ javaCompleto2020JDBC/src/application/Program.java
@@ -28,9 +28,16 @@ public class Program {
 
         System.out.println("\n" + "=".repeat(10) + " TEST 4: seller insert " + "=".repeat(10));
         Department noNameDep = new Department(2, null);
-        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", LocalDate.of(2020, 1, 1), 4000.0,
+        seller = new Seller(null, "Greg", "greg@gmail.com", LocalDate.of(2020, 1, 1), 4000.0,
                 noNameDep);
-        sellerDao.insert(newSeller);
-        System.out.println("Inserted! New id = " + newSeller.getId());
+        sellerDao.insert(seller);
+        System.out.println("Inserted! New id = " + seller.getId());
+        
+        System.out.println("\n" + "=".repeat(10) + " TEST 5: seller update " + "=".repeat(10));
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Wayne");
+        sellerDao.update(seller);
+        System.out.println("Update completed!");
+        
     }
 }
